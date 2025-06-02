9e5ddd303fa2181af31a6ae49d1bf1747a60242f
Martin gago <martin.gago@flowreserve.es>
2025-06-02 08:26:27 +0200

    +arreglo footer versiones moviles

--- src/components/FooterWeb/FooterWeb.tsx
+++ src/components/FooterWeb/FooterWeb.tsx
@@ -6,10 +6,10 @@ const FooterWeb: FC = () => {
     return (
         <footer className="flex flex-col items-center p-5">
             <Link to="/">
-                <img src="/web/flowreserve-logo.png" alt="Logo" className="w-[300px]" />
+                <img src="/web/flowreserve-logo.png" alt="Logo" className="w-[90%] m-auto  sm:w-[300px]" />
             </Link>
 
-            <nav className="flex items-center gap-8">
+            <nav className="flex items-center flex-col md:flex-row gap-8">
                 <a href="https://www.google.es" target="_blank" rel="noopener noreferrer">Inicio</a>
                 <a href="https://www.google.es" target="_blank" rel="noopener noreferrer">Contacto</a>
                 <a href="https://www.google.es" target="_blank" rel="noopener noreferrer">Ayuda</a>
